dc41f8e348a043b96424bbff0d3ba5373b4e97c8
harlan0103 <haoranliang0103@gmail.com>
2022-08-19 20:53:55 +0800

    Init

---
+++ KeyBoardSimulator/KeyBoardSimulator.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+#include <string>
+
+
+int main() {
+	std::cout << "Waiting..." << std::endl;
+
+	Sleep(10000);
+
+	std::cout << "START" << std::endl;
+
+	while (true) {
+		// 退出篝火
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+		Sleep(50);
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+		Sleep(4000);
+
+		// 五次陨石咒
+		for (int i = 0; i < 5; i++) {
+			mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+			Sleep(50);
+			mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+			Sleep(4000);
+		}
+
+		// 进入篝火
+		mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0);
+		Sleep(50);
+		mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0);
+		Sleep(3500);
+	}
+}
